35070e9f4e6590f6b2366bf9f2175fe4306edd52
Andres <andres99.dev@gmail.com>
2025-11-14 21:47:54 +0100

    add redis listeners check

--- src/server/redis.ts
+++ src/server/redis.ts
@@ -4,3 +4,10 @@ import { Redis } from 'ioredis'
 export const redis = new Redis(env.REDIS_URL)
 
 export const PLAYER_STATE_KEY = (userId: string) => `player:${userId}:state`
+setInterval(() => {
+  console.log(
+    'redis listeners',
+    redis.listenerCount('end'),
+    redis.listenerCount('error')
+  )
+}, 30000)
